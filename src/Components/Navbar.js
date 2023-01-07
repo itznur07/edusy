acf2a72c9bf7276a9e8150df1f7f8d99df6a504d
Md Nuruddin <nuruddinalways99@gmail.com>
2023-01-07 22:52:04 +0600

    Navbar Component Desing Done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,66 +1,26 @@
-import React from "react";
+import React from 'react'
 
-const TopBar = () => {
+const Navbar = () => {
   return (
     <>
-      <div
-        style={{
-          backgroundImage: `url("https://preview.colorlib.com/theme/academia/images/bg_1.jpg")`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          width: "100%",
-          height: "140vh",
-        }}
-      >
-        <div className='mx-24'>
-          <div className='flex items-center justify-between space-x-24 py-6'>
-            {/* sec 1 */}
-            <div>
-              <h1 className='text-3xl font-bold uppercase text-white cursor-pointer'>Edusy</h1>
-              <p className='text-sm text-white'>ONLINE EDUCATION & LEARNING</p>
-            </div>
-            {/* sec 2 */}
-            <div className='flex items-center space-x-3 text-white'>
-              <span className='text-4xl'>
-                <i className='fas fa-clock'></i>
-              </span>
-              <div>
-                <span className='text-md'>Monday - Friday</span>
-                <br />
-                <strong>8:00AM-8:00PM</strong>
-              </div>
-            </div>
-            {/* sec 3 */}
-            <div className='flex items-center space-x-3 text-white'>
-              <span className='text-4xl'>
-                <i className='fas fa-phone'></i>
-              </span>
-              <div>
-                <span className='text-md'>Call Us</span>
-                <br />
-                <strong>+2 392 3929 210</strong>
-              </div>
-            </div>
-            {/* sec 4 */}
-            <div className='space-x-6 text-md cursor-pointer'>
-              <span className='bg-[#fff] hover:bg-[#1EB2A6] bg-opacity-30 hover:text-white text-[#1EB2A6] px-4 py-2.5 rounded-full '>
-                <i class='fa-brands fa-facebook-f'></i>
-              </span>
-              <span className='bg-[#fff] hover:bg-[#1EB2A6] bg-opacity-30 hover:text-white text-[#1EB2A6] px-4 py-3 rounded-full '>
-                <i className='fab fa-youtube'></i>
-              </span>
-              <span className='bg-[#fff] hover:bg-[#1EB2A6] bg-opacity-30 hover:text-white text-[#1EB2A6] px-4 py-3 rounded-full '>
-                <i className='fab fa-twitter'></i>
-              </span>
-              <span className='bg-[#fff] hover:bg-[#1EB2A6] bg-opacity-30 hover:text-white text-[#1EB2A6] px-4 py-3 rounded-full '>
-                <i className='fab fa-instagram'></i>
-              </span>
-            </div>
-          </div>
+    <div className='mx-24'>
+        <div className='flex items-center justify-between px-6 py-6 bg-[#fff] bg-opacity-10 cursor-pointer'>
+            <nav>
+                <ul className='flex items-center space-x-14 text-white text-base font-medium'>
+                    <li className='text-[#1EB2A6]'>Home</li>
+                    <li>All Courses</li>
+                    <li>About</li>
+                    <li>Team</li>
+                    <li>Pricing</li>
+                    <li>Journal</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            <div ><p className='text-base text-[#1eb2a6] text-lg font-bold rounded-md'>Get Certificate</p></div>
         </div>
-      </div>
+    </div>
     </>
-  );
-};
+  )
+}
 
-export default TopBar;
+export default Navbar
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import TopBar from '../Components/Navbar'
+import Navbar from '../Components/Navbar'
+import TopBar from '../Components/TopBar'
 
 const Home = () => {
   return (
     <>
-        <TopBar/>
+    {/* TopBar and Navbar */}
+    <div style={{
+          backgroundImage: `url("https://preview.colorlib.com/theme/academia/images/bg_1.jpg")`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          width: "100%",
+          height: "140vh",
+        }}>
+      <TopBar/>
+      <Navbar/>
+    </div>
+        
     </>
   )
 }
